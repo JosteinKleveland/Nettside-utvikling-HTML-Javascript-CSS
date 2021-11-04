0d66f1321c5e880437b0e7f8579c9c4209159cb7
Jon <jonph@stud.ntnu.no>
2021-11-04 20:18:19 +0100

    Kart rework

--- javaScript/hotspots.js
+++ javaScript/hotspots.js
@@ -2,13 +2,13 @@
 const icons = document.querySelectorAll(".mapIcon");
 const inngang = document.getElementById("inngangIcon");
 const box = document.getElementsByClassName("iconText");
-
-
-
-const divs = document.querySelectorAll('.a');
+const closeButton = document.querySelectorAll(".closeMap");
 
 icons.forEach(el => el.addEventListener('click', event => {
  openMapInfo(event.currentTarget.getAttribute("iconTarget"))}));
+
+closeButton.forEach(el => el.addEventListener('click', event => {
+    closeMapInfo(event.currentTarget.closest(".iconText").id)}));
 
 
 function openMapInfo(infoID) {
@@ -16,8 +16,8 @@ function openMapInfo(infoID) {
     console.log("open")
   }
   
-function closeMapInfo() {
-    document.box.style.display = "none";
+function closeMapInfo(infoID) {
+    document.getElementById(infoID).style.display = "none";
     console.log("close")
   }
 
